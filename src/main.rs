9c9f131bcacc32b04aca7267ebc215d5308b88b7
Armando Perez <gmandx@gmail.com>
2018-07-05 15:39:34 -0700

    Extract most urls, and show the URL selection menu

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,36 @@ use std::collections::VecDeque;
 
 use dialoguer::Select;
 use failure::Error;
-use url::{Url, ParseError};
+use url::Url;
 
-fn sub_parse(url: &str) -> Option<Url> {
-    match Url::parse(url) {
-        Ok(parsed) => {
-            Some(parsed)
-        },
-        Err(_) => None,
+fn main() -> Result<(), Error> {
+    let mut urls: Vec<Url> = Vec::new();
+    let mut queue: VecDeque<Url> = VecDeque::new();
+
+    std::env::args()
+        .skip(1)
+        .for_each(|arg| match Url::parse(&arg) {
+            Ok(url) => queue.push_back(url),
+            Err(_) => println!("Could not parse {:?} as URL", arg),
+        });
+
+    while let Some(url) = queue.pop_front() {
+        for (_, value) in url.query_pairs() {
+            if let Ok(url) = Url::parse(&value) {
+                queue.push_back(url);
+            }
+        }
+        urls.push(url);
     }
-}
 
-fn main() {
+    if urls.len() < 1 {
+        eprintln!("No URLs detected");
+    }
 
-    let urls: Vec<Url> = Vec::new();
-    let queue: VecDeque<Url> = VecDeque::new();
+    let options = urls.iter().map(|url| url.as_str()).collect::<Vec<_>>();
+    let selection = Select::new().items(&options).interact()?;
 
-    std::env::args().skip(1).for_each(|arg| match Url::parse(&arg) {
-        Ok(url) => queue.push(),
-        Err(_) => {
-            println!("Could not parse {:?} as URL", arg);
-            None
-        },
-    });
+    println!("Chosen {:?}", options[selection]);
 
-    println!("{:?}", urls.collect::<Vec<Url>>());
+    Ok(())
 }
